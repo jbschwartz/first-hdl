5eaa90c7a9c2d9d989536b9f9515f881c89459b4
James Schwartz <jbschwartz@users.noreply.github.com>
2022-05-08 12:58:54 -0400

    Implement registers.

--- src/register.v
+++ src/register.v
@@ -9,3 +9,32 @@ module dff(
     end
 
 endmodule
+
+module bit(
+    input wire clk,
+    input wire load,
+    input wire in,
+    output wire out
+    );
+
+    wire mux_out;
+
+    mux mux(.a(out), .b(in), .sel(load), .out(mux_out));
+    dff dff(.clk(clk), .d(mux_out), .q(out));
+
+endmodule
+
+module register #(
+    parameter N = 32
+    ) (
+    input wire clk,
+    input wire load,
+    input wire [N-1:0] in,
+    output wire [N-1:0] out
+    );
+
+    genvar i;
+
+    for(i = 0; i < N; i = i + 1) bit bit(.clk(clk), .load(load), .in(in[i]), .out(out[i]));
+
+endmodule
--- tests/register_tb.v
+++ tests/register_tb.v
@@ -1,17 +1,30 @@
 `timescale 1ns / 1ps
 
 module register_tb;
+    localparam N = 32;
+
     reg clk;
     reg d;
+    reg in;
+    reg load;
+    reg [N-1:0] reg_in;
+    reg reg_load;
+
+    wire out;
+    wire [N-1:0] reg_out;
 
     dff dff(.clk(clk), .d(d), .q(q));
+    bit bit(.clk(clk), .in(in), .load(load), .out(out));
+    register register(.clk(clk), .in(reg_in), .load(reg_load), .out(reg_out));
 
     initial begin
-        clk = 1; d = 0;
+        clk = 1;
         forever #1 clk = ~clk;
     end
 
+    // DFF
     initial begin
+        d = 0;
         #2
         if(q !== 0) $display("[FAILED] DFF != 0 (got %d)", q);
 
@@ -23,6 +36,29 @@ module register_tb;
 
         #2.5 d = 0; #1
         if(q !== 0) $display("[FAILED] DFF != 0 (got %d)", q);
+    end
+
+    // Bit
+    initial begin
+        in = 0; load = 1;
+        #2.5 load = 0;
+        if(out !== 0) $display("[FAILED] BIT != 0 (got %d)", q);
+        #2.5 in = 1; #2.5
+        if(out !== 0) $display("[FAILED] BIT != 0 (got %d)", q);
+        #2.5 load = 1; #2.5
+        if(out !== 1) $display("[FAILED] BIT != 1 (got %d)", q);
+    end
+
+    // Register
+    initial begin
+        reg_in = 32'b0; reg_load = 1;
+        #2.5 reg_load = 0;
+        if(reg_out !== 32'b0) $display("[FAILED] REGISTER != 0 (got %h)", reg_out);
+        #2.5 reg_in = 32'hFFFFAAAA;
+        reg_load = 1; #2; reg_load = 0; #5
+        if(reg_out !== 32'hFFFFAAAA) $display("[FAILED] REGISTER != FFFFAAAA (got %h)", out);
+        #5 reg_in = 32'b0; #5
+        if(reg_out !== 32'hFFFFAAAA) $display("[FAILED] REGISTER != FFFFAAAA (got %h)", out);
     end
 
 endmodule
